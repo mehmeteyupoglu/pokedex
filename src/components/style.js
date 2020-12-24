ba2a3b5b1df3557f518d84da11d152fab3441d03
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-12-24 22:52:59 +0300

    Cleaner styling

---
+++ src/components/style.js
@@ -0,0 +1,11 @@
+export const cardStyle = {
+  margin: "10px",
+  height: "35rem",
+  width: "20rem",
+  boxShadow: "1px 1px 2px gray",
+  border: "none",
+  cursor: "pointer",
+  "&:hover": {
+    backgroundColor: "blue",
+  },
+};
