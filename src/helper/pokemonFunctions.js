eda02763229908e608c2e29232f0502c4d0670d8
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-12-24 18:08:02 +0300

    Add more helper functions for various use cases

--- src/helper/pokemonFunctions.js
+++ src/helper/pokemonFunctions.js
@@ -17,3 +17,33 @@ export async function getIndividualPokemons(url) {
       });
   });
 }
+
+export function filterPokemons(id, pokemons) {
+  const filteredPokemons = pokemons.filter((item) => {
+    return item.id === id;
+  });
+}
+
+export const renderAbilities = (arr, index) => {
+  let abilities = arr.map((item) => {
+    return item.ability.name;
+  });
+
+  return abilities.join(", ");
+};
+
+export const renderTypes = (arr, index) => {
+  let types = arr.map((item) => {
+    return item.type.name;
+  });
+
+  return types.join(", ");
+};
+
+export const renderMoves = (arr, index) => {
+  let moves = arr.slice(0, 10).map((item) => {
+    return item.move.name;
+  });
+
+  return moves.join(", ");
+};
